fafdd23f599a8529bd18d67f2a024869ea87fb99
matias <csagastume@fi.uba.ar>
2025-05-17 20:57:06 -0300

    Agrego funcion list matches del lobby

--- server/PlayerCommand.h
+++ server/PlayerCommand.h
@@ -24,7 +24,7 @@ public:
     const PlayerCredentials credentials;
     const CommandTypes command_type;
 
-    PlayerCommand(const PlayerCredentials& credentials, const CommandTypes type);
+    PlayerCommand(const PlayerCredentials& credentials, CommandTypes type);
 };
 
 
--- server/match_dto.h
+++ server/match_dto.h
@@ -5,13 +5,17 @@
 #ifndef MATCH_DTO_H
 #define MATCH_DTO_H
 #include <string>
-
+#include <utility>
+#define generic_map_name "test_map"
 class MatchDTO {
 public:
     const std::string match_name;
+    const std::string map_name;
     const int number_of_players;
-    MatchDTO(const std::string& match_name, const int number_of_players):
-            match_name(match_name), number_of_players(number_of_players) {}
+    MatchDTO(std::string match_name, const int number_of_players):
+            match_name(std::move(match_name)),
+            map_name(generic_map_name),
+            number_of_players(number_of_players) {}
 };
 
 #endif  // MATCH_DTO_H
