b514fce1e677462524727974b94742e5e868cb4f
martin saavedra <martin996@hotmail.com.ar>
2025-05-17 19:19:59 -0300

    no consideraba el largo del payload

--- common/dto/player_action.h
+++ common/dto/player_action.h
@@ -17,15 +17,17 @@ private:
     MessageType message_type = MessageType::PlayerAction;
     Action action;
 public:
-    PlayerAction(Action action)
-        : action(action) {}
+    PlayerAction(Action action): action(action) {}
+
     void serialize(uint8_t* buffer) const override {
         buffer[0] = static_cast<uint8_t>(message_type);
-        buffer[1] = static_cast<uint8_t>(action);
+        uint16_t payload_length = htons(1);
+        std::memcpy(buffer + 1, &payload_length, sizeof(payload_length));
+        buffer[3] = static_cast<uint8_t>(action);
     }
 
     size_t serialized_size() const override {
-        return 2;
+        return 4;
     }
 
     uint8_t get_action() const {
@@ -33,10 +35,10 @@ public:
     }
 
     static PlayerAction deserialize(const uint8_t* buffer, size_t size) {
-        if (size < 3) {
+        if (size < 4) {
             throw std::runtime_error("");
         }
-        uint8_t action_deserialized = buffer[1];
+        uint8_t action_deserialized = buffer[3];
         return PlayerAction(static_cast<Action>(action_deserialized));
     }
 
