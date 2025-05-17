10e9814ecbac6d448955b2fdd9c14785f503723e
matias <csagastume@fi.uba.ar>
2025-05-17 20:22:12 -0300

    Agrego cobertura a caso borde en el cual ocurre un error en el sender para abandonar partida

--- server/sender.h
+++ server/sender.h
@@ -8,12 +8,19 @@
 #include "common/catedra/thread.h"
 #include "common/protocol.h"
 
+#include "client_handler.h"
 #include "match_status_dto.h"
 
 class Sender: public Thread {
-    Protocol protocol;
-    Queue<MatchStatusDTO> sender_queue;
+    Protocol& protocol;
+    Queue<MatchStatusDTO>& sender_queue;
+    ClientHandler& client_handler;
 
+    void notify_error_to_client_handler();
+
+public:
+    Sender(Protocol& protocol, Queue<MatchStatusDTO>& sender_queue, ClientHandler& client_handler):
+            protocol(protocol), sender_queue(sender_queue), client_handler(client_handler) {}
     void run() override;
     void stop() override;
 };
