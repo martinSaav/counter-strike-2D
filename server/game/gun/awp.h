930d00edf46e2bfd28c3a8707b43ec9bb909ef4b
matias <csagastume@fi.uba.ar>
2025-06-03 18:50:09 -0300

    agrego planteo de armas, con parte del ak-47 ya implementado

---
+++ server/game/gun/awp.h
@@ -0,0 +1,15 @@
+//
+// Created by matias on 03/06/25.
+//
+
+#ifndef AWP_H
+#define AWP_H
+#include "gun.h"
+
+
+class Awp: public Gun {
+    GunType type = GunType::Primary;
+};
+
+
+#endif  // AWP_H
