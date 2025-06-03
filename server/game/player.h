712ca88910e0c1034a50fa8687c47b2690a6a668
matias <csagastume@fi.uba.ar>
2025-06-03 17:12:47 -0300

    agrego skin y angulo

--- server/game/player.h
+++ server/game/player.h
@@ -6,24 +6,33 @@
 
 #include "../player_dto.h"
 
+#include "player_skin.h"
 #include "position.h"
 #define player_hitbox_height 32
 #define player_hitbox_width 32
 
 class Player {
     const std::string username;
+    PlayerSkin skin;
+    int current_angle;
     int position_x;
     int position_y;
     std::vector<std::pair<int, int>>
             chunks_idxs;  // indices de los chunks en los que se encuentra el jugador
 
 public:
-    Player(const std::string& username, const int position_x, const int position_y):
-            username(username), position_x(position_x), position_y(position_y) {
+    Player(std::string username, const int position_x, const int position_y):
+            username(std::move(username)),
+            current_angle(0),
+            position_x(position_x),
+            position_y(position_y) {
+        skin = PlayerSkin::C1;
         chunks_idxs.emplace_back(0, 0);
     }
     std::pair<int, int> get_location();
     void set_location(Position position, std::vector<std::pair<int, int>>&& chunks_idxs);
+    void set_angle(const int angle) { current_angle = angle; }
+    void set_skin(const PlayerSkin skin) { this->skin = skin; }
     [[nodiscard]] PlayerDTO get_player_info() const;
 
     std::vector<std::pair<int, int>>& get_chunk_idxs() { return chunks_idxs; }
--- server/player_dto.h
+++ server/player_dto.h
@@ -2,13 +2,23 @@
 #define PLAYERDTO_H
 #include <string>
 #include <utility>
+
+#include "player_skin.h"
+
 class PlayerDTO {
 public:
     const std::string username;
+    const PlayerSkin skin;
+    const int angle;
     const int position_x;
     const int position_y;
-    PlayerDTO(std::string username, const int position_x, const int position_y):
-            username(std::move(username)), position_x(position_x), position_y(position_y) {}
+    PlayerDTO(std::string username, const int position_x, const int position_y,
+              const PlayerSkin skin, const int angle):
+            username(std::move(username)),
+            skin(skin),
+            angle(angle),
+            position_x(position_x),
+            position_y(position_y) {}
 };
 
 #endif  // PLAYERDTO_H
