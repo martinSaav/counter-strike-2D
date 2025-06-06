53aae1a6da715d2830dac075d4e2b6b6ad0c69e7
matias <csagastume@fi.uba.ar>
2025-06-06 15:59:58 -0300

    agrego game manager

--- server/game/game_manager.cpp
+++ server/game/game_manager.cpp
@@ -3,15 +3,3 @@
 //
 
 #include "game_manager.h"
-
-
-void GameManager::attack_player(const std::shared_ptr<Player>& attacked, Player& attacker,
-                                const int damage) {
-    if (attacked->current_team == attacker.current_team) {
-        return;
-    }
-    attacked->receive_damage(damage);
-    if (attacked->is_dead()) {
-        attacker.add_kill();
-    }
-}
--- server/game/game_manager.h
+++ server/game/game_manager.h
@@ -10,15 +10,10 @@
 
 
 class GameManager {
-    Map& map;
-    float time;
-
 public:
-    explicit GameManager(Map& map): map(map), time(0) {}
-    void attack_player(const std::shared_ptr<Player>& attacked, Player& attacker, int damage);
-    void advance_time(const float dt) { time += dt; }
-    float get_time() const { return time; }
-    Map& get_map() const { return map; }
+    GameManager();
+    void attack_player(std::shared_ptr<Player> attacked, std::shared_ptr<Player> attacker,
+                       int damage);
 };
 
 
