65fa626c7833d301735bf7fbf5d89e2e4497617a
martin saavedra <martin996@hotmail.com.ar>
2025-06-09 19:35:01 -0300

    no pasa el arma en cada accion

--- common/dto/player_action.h
+++ common/dto/player_action.h
@@ -21,11 +21,10 @@ private:
     Action action;
     uint16_t pos_mouse_x;
     uint16_t pos_mouse_y;
-    Weapon weapon;
 
 public:
-    explicit PlayerAction(Action action, uint16_t pos_mouse_x, uint16_t pos_mouse_y, Weapon weapon):
-            action(action), pos_mouse_x(pos_mouse_x), pos_mouse_y(pos_mouse_y), weapon(weapon) {}
+    explicit PlayerAction(Action action, uint16_t pos_mouse_x, uint16_t pos_mouse_y):
+            action(action), pos_mouse_x(pos_mouse_x), pos_mouse_y(pos_mouse_y) {}
 
     void serialize(uint8_t* buffer) const override {
         buffer[0] = static_cast<uint8_t>(message_type);
@@ -36,15 +35,13 @@ public:
         uint16_t pos_mouse_y_be = htons(pos_mouse_y);
         std::memcpy(buffer + 4, &pos_mouse_x_be, sizeof(pos_mouse_x_be));
         std::memcpy(buffer + 6, &pos_mouse_y_be, sizeof(pos_mouse_y_be));
-        buffer[8] = static_cast<uint8_t>(weapon);
     }
 
-    size_t serialized_size() const override { return 9; }
+    size_t serialized_size() const override { return 8; }
 
     Action get_action() const { return action; }
     uint16_t get_pos_mouse_x() const { return pos_mouse_x; }
     uint16_t get_pos_mouse_y() const { return pos_mouse_y; }
-    Weapon get_weapon() const { return weapon; }
 
     static PlayerAction deserialize(const uint8_t* buffer, size_t size) {
         if (size < 4) {
@@ -56,9 +53,7 @@ public:
         std::memcpy(&pos_mouse_y, buffer + 6, sizeof(pos_mouse_y));
         pos_mouse_x = ntohs(pos_mouse_x);
         pos_mouse_y = ntohs(pos_mouse_y);
-        uint8_t weapon_deserialized = buffer[8];
-        return PlayerAction(static_cast<Action>(action_deserialized), pos_mouse_x, pos_mouse_y,
-                            static_cast<Weapon>(weapon_deserialized));
+        return PlayerAction(static_cast<Action>(action_deserialized), pos_mouse_x, pos_mouse_y);
     }
 
     MessageType type() const override { return this->message_type; }
