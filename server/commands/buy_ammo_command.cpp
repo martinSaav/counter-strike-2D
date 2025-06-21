4325452b8ecfbebf73b06508dda9687bf678ab75
matias <csagastume@fi.uba.ar>
2025-06-20 21:49:10 -0300

    -

--- server/commands/buy_ammo_command.cpp
+++ server/commands/buy_ammo_command.cpp
@@ -4,8 +4,12 @@
 
 #include "buy_ammo_command.h"
 
+#include "request_processor.h"
+
 BuyAmmoCommand::BuyAmmoCommand(const PlayerCredentials credentials, const GunType weapon_type):
         credentials(credentials), weapon_type(weapon_type) {}
 
 
-void BuyAmmoCommand::process_command(Match* match) {}
+void BuyAmmoCommand::process_command(Match* match) {
+    RequestProcessor::process_buy_ammo_request(credentials, weapon_type, match);
+}
