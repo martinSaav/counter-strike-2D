076fdc9738a65185c56bc2d7f1022c3a379cbfb0
matias <csagastume@fi.uba.ar>
2025-06-22 21:01:53 -0300

    agrego cooldown para todas las armas

--- server/gun_config.h
+++ server/gun_config.h
@@ -18,19 +18,9 @@ public:
     const int shoot_cooldown;
     const int range;
     const double angle;
-    GunConfig(int max_ammo, int starting_reserve_ammo, int min_dmg, int max_dmg, int gun_price):
-            max_ammo(max_ammo),
-            starting_reserve_ammo(starting_reserve_ammo),
-            min_dmg(min_dmg),
-            max_dmg(max_dmg),
-            gun_price(gun_price),
-            bullets_per_burst(0),
-            shoot_cooldown(0),
-            range(0),
-            angle(0) {}
 
     GunConfig(int max_ammo, int starting_reserve_ammo, int min_dmg, int max_dmg, int gun_price,
-              int bullets_per_burst, int shoot_cooldown):
+              int range, double angle, int shoot_cooldown, int bullets_per_burst):
             max_ammo(max_ammo),
             starting_reserve_ammo(starting_reserve_ammo),
             min_dmg(min_dmg),
@@ -38,41 +28,6 @@ public:
             gun_price(gun_price),
             bullets_per_burst(bullets_per_burst),
             shoot_cooldown(shoot_cooldown),
-            range(0),
-            angle(0) {}
-
-    GunConfig(int max_ammo, int starting_reserve_ammo, int min_dmg, int max_dmg, int gun_price,
-              int shoot_cooldown):
-            max_ammo(max_ammo),
-            starting_reserve_ammo(starting_reserve_ammo),
-            min_dmg(min_dmg),
-            max_dmg(max_dmg),
-            gun_price(gun_price),
-            bullets_per_burst(0),
-            shoot_cooldown(shoot_cooldown),
-            range(0),
-            angle(0) {}
-
-    GunConfig(int min_dmg, int max_dmg, int range, double angle):
-            max_ammo(0),
-            starting_reserve_ammo(0),
-            min_dmg(min_dmg),
-            max_dmg(max_dmg),
-            gun_price(0),
-            bullets_per_burst(0),
-            shoot_cooldown(0),
-            range(range),
-            angle(angle) {}
-
-    GunConfig(int max_ammo, int starting_reserve_ammo, int min_dmg, int max_dmg, int gun_price,
-              int range, double angle):
-            max_ammo(max_ammo),
-            starting_reserve_ammo(starting_reserve_ammo),
-            min_dmg(min_dmg),
-            max_dmg(max_dmg),
-            gun_price(gun_price),
-            bullets_per_burst(0),
-            shoot_cooldown(0),
             range(range),
             angle(angle) {}
     [[nodiscard]] GunConfigInfo get_info() const {
